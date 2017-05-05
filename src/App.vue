edd7160039fafc9c59d54cb0f0a8aef42a0355a9
M-Hc0d3 <mowz971@gmail.com>
2017-05-05 04:10:57 +0100

    appheader router added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
+  <div>
+    <app-header></app-header>
+    <img src="./assets/logo.png">
     <router-view></router-view>
+  </div>
 </template>
 
 <script>
+import appHeader from './components/appHeader.vue'
 
+export default {
+  components: {
+    appHeader: appHeader
+  }
+}
 </script>
 
 <style>
