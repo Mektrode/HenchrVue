5c4023f8c6085155788994d35579179a26d75b4b
M-Hc0d3 <mowz971@gmail.com>
2017-05-07 17:07:49 +0100

    aMeal.vue & Meals.vue component interaction
    Fake data meal object for now to test up comms between components

--- src/components/Meals/Meals.vue
+++ src/components/Meals/Meals.vue
@@ -1,3 +1,26 @@
 <template>
-<h1>View your meals</h1>
+    <div>
+        <h1>View your meals</h1>
+        <another-Meal v-for="meal in meals" :meal="meal"></another-Meal>
+    </div>
 </template>
+
+<script>
+    import MealW from './aMeal.vue'
+
+    export default {
+        data () {
+            return {
+                meals: [
+                    {id: 1, name: 'Oats Shake', calories: 380, carbs: 120, prot: 29, calrPerc: 2},
+                    {id: 2, name: 'Meal Deal', calories: 704, carbs: 230, prot: 67, calrPerc: 22},
+                    {id: 3, name: 'Bueno', calories: 89, carbs: 3, prot: 0.9, calrPerc: 19},
+                    {id: 4, name: 'Jerk Wrap', calories: 405, carbs: 120, prot: 24, calrPerc: 900}
+                ]
+            }
+        },
+        components: {
+            anotherMeal: MealW
+        }
+    }
+</script>
---
+++ src/components/Meals/aMeal.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+    <h3>
+        {{ meal.name }}
+        <small>(Added a good {{ meal.calrPerc }}% today)</small>
+    </h3>
+    <p>
+        Calories: <strong>{{meal.calories}}</strong>kJ
+    </p>
+    <p>
+        Carbohydrates: <strong>{{meal.carbs}}</strong>g
+    </p>
+    <p>
+        Protien: <strong>{{meal.prot}}</strong>g
+    </p>
+</div>
+</template>
+<script>
+    export default {
+        props: ['meal']
+    }
+</script>
+
