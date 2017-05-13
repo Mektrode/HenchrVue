c6f4be87e78418f2858055e3d3e456745f427f75
mowzchill <mowz971@gmail.com>
2017-05-13 22:51:46 +0000

    Vue Material UI

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueMaterial from 'vue-material'
 
+import 'vue-material/dist/vue-material.css'
 import App from './App'
 import {routes} from './routes'
 
 Vue.use(VueRouter)
+Vue.use(VueMaterial)
 
 const router = new VueRouter({
     mode: 'history',
