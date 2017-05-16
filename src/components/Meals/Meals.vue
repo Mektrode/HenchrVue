478df67e32052cc250b4907bba9347244eb600a8
mowzchill <mowz971@gmail.com>
2017-05-16 22:33:15 +0000

    some more fake data

--- src/components/Meals/Meals.vue
+++ src/components/Meals/Meals.vue
@@ -12,10 +12,13 @@
         data () {
             return {
                 meals: [
-                    {id: 1, name: 'Oats Shake', calories: 380, carbs: 120, prot: 29, calrPerc: 2},
-                    {id: 2, name: 'Meal Deal', calories: 704, carbs: 230, prot: 67, calrPerc: 22},
-                    {id: 3, name: 'Bueno', calories: 89, carbs: 3, prot: 0.9, calrPerc: 19},
-                    {id: 4, name: 'Jerk Wrap', calories: 405, carbs: 120, prot: 24, calrPerc: 900}
+                    {id: 1, name: 'Oats Shake', category: 'Breakfast', calories: 380, carbs: 120, prot: 29, calrPerc: 2},
+                    {id: 2, name: 'Meal Deal', category: 'Lunch', calories: 704, carbs: 230, prot: 67, calrPerc: 22},
+                    {id: 3, name: 'Bueno', category: 'Snack', calories: 89, carbs: 3, prot: 0.9, calrPerc: 19},
+                    {id: 4, name: 'Jerk Rice', category: 'Dinner', calories: 405, carbs: 120, prot: 24, calrPerc: 900},
+                    {id: 5, name: 'Cereals', category: 'Breakfast', calories: 380, carbs: 120, prot: 29, calrPerc: 2},
+                    {id: 6, name: 'Oats Shake', category: 'breakfast', calories: 380, carbs: 120, prot: 29, calrPerc: 20},
+                    {id: 7, name: 'Oats Shake', category: 'breakfast', calories: 380, carbs: 120, prot: 29, calrPerc: 32}
                 ]
             }
         },
