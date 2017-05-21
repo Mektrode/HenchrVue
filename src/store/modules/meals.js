5b8858aff82cdc5bc0213d67172d35a9bb38023a
mowzchill <mowz971@gmail.com>
2017-05-21 22:52:50 +0000

    starting on vuexin the meals

--- src/store/modules/meals.js
+++ src/store/modules/meals.js
@@ -1,42 +1,80 @@
-import axios from "axios"
+import axios from 'axios'
 
 const state = {
-    meals: []
+    meals: [
+        {
+            id: 21,
+            name: 'Meal Deal',
+            category: 'Lunch',
+            inside: {
+                item1: {
+                    id: 21,
+                    name: 'Bueno',
+                    category: 'Snack',
+                    calories: 89,
+                    carbs: 3,
+                    prot: 0.9,
+                    calrPerc: 19,
+                    proPerc: 1,
+                    carbPerc: 2
+                },
+                item2: {
+                    id: 22,
+                    name: 'Bueno',
+                    category: 'Snack',
+                    calories: 89,
+                    carbs: 3,
+                    prot: 0.9,
+                    calrPerc: 19,
+                    proPerc: 1,
+                    carbPerc: 2
+                }
+            },
+            stats: {
+                calories: 704,
+                carbs: 230,
+                prot: 67,
+                calrPerc: 22,
+                proPerc: 42,
+                carbPerc: 51
+            }
+        }
+    ]
 }
 
 const actions = {
     LOAD_meal_LIST: function ({ commit }) {
-      axios.get('/secured/meals').then((response) => {
-        commit('SET_meal_LIST', { list: response.data })
-      }, (err) => {
-        console.log(err)
-      })
+        axios.get('/secured/meals').then((response) => {
+            commit('SET_meal_LIST', { list: response.data })
+        }, (err) => {
+            console.log(err)
+        })
     },
     ADD_NEW_meal: function ({ commit }) {
-      axios.post('/secured/meals').then((response) => {
-        commit('ADD_meal', { meal: response.data })
-      }, (err) => {
-        console.log(err)
-      })
+        axios.post('/secured/meals').then((response) => {
+            commit('ADD_meal', { meal: response.data })
+        }, (err) => {
+            console.log(err)
+        })
     }
 }
 
 const mutations = {
     SET_meal_LIST: (state, { list }) => {
-      state.meals = list
+        state.meals = list
     },
     ADD_meal: (state, { meal }) => {
-      state.meals.push(meal)
+        state.meals.push(meal)
     },
     UPDATE_meal: (state, { item }) => {
-      let idx = state.meals.map(p => p.id).indexOf(item.id)
-      state.meals.splice(idx, 1, item)
+        let idx = state.meals.map(p => p.id).indexOf(item.id)
+        state.meals.splice(idx, 1, item)
     }
 }
 
 const getters = {
     mealCount: state => {
-      return state.meals.length
+        return state.meals.length
     }
 }
 
