5c49131f7b835136684d957fce72af4aac65b922
mowzchill <mowz971@gmail.com>
2017-05-22 15:14:00 +0000

    disptach meal test

--- src/components/gather/NewMeal.vue
+++ src/components/gather/NewMeal.vue
@@ -9,7 +9,7 @@
         <input type="number" v-model="mealCarb" placeholder="Carbohydrates?">
         <label>Protein (in grams):</label>
         <input type="number" v-model="mealProt" placeholder="Protein?">
-        <button type="submit" @click="done">Submit</button>
+        <button type="submit" @click="addameal">Submit</button>
         
     </div>
 </template>
@@ -19,6 +19,15 @@ export default {
     methods: {
         done () {
             alert('You submitted: ' + this.mealName + ': ' + this.mealCal + 'kJ , with ' + this.mealCarb + 'g of CARBS, ' + this.mealProt + 'g of PROT')
+        },
+        addameal (mealName, mealCal, mealCarb, mealProt) {
+            /* const meal = {
+                mealName: this.meal.name,
+                mealCal: this.meal.info.calories,
+                mealCarb: this.meal.info.carbs,
+                mealProt: this.meal.info.prot
+            } */
+            this.$store.dispatch('adddmeal', {mealName, mealCal, mealCarb, mealProt})
         }
     }
 
